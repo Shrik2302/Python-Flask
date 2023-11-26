1952454a453977df438e6377c4393e4864eb82bb
Shrik2302 <53657697+Shrik2302@users.noreply.github.com>
2023-11-26 17:45:37 +0530

    Update insert.py

--- PostgreSQL_CRUD_Operations/insert.py
+++ PostgreSQL_CRUD_Operations/insert.py
@@ -1,10 +1,10 @@
 import psycopg2
 try:
-    connection = psycopg2.connect(user='postgres',
-                                  password='root',
-                                  host='localhost',
-                                  port='5432',
-                                  database='basic')
+    connection = psycopg2.connect(user='postgres_username',
+                                  password='postgres_user_passwerd',
+                                  host='host_name',
+                                  port='port_number',
+                                  database='database_name')
     cursor = connection.cursor()
     query = ("INSERT INTO employee VALUES (%s,%s,%s,%s,%s,%s)")
     values = (16, 'ajay', 'lead', '2022-06-09', '15:00:00', '{"name":"ajay","role":"lead"}')
