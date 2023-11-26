f6171233828d18e7edbbd81606c4b89e953a1400
Shrik2302 <53657697+Shrik2302@users.noreply.github.com>
2023-11-26 17:33:22 +0530

    Update config.py

--- Mail_Project/config.py
+++ Mail_Project/config.py
@@ -4,7 +4,7 @@ from app import app
 # configuration of mail
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'shrikant99.mane@gmail.com'
+app.config['MAIL_USERNAME'] = 'sender_mail_address'
 app.config['MAIL_PASSWORD'] = 'Provide_Mail_Password_Here'
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
